b9d7bbe75b1c323639dcacf7cf5d4b897aa6d14f
Lena <1153309@cegepmontpetit.ca>
2022-06-29 13:32:12 -0400

    FCT CalendarEvent
    Création modèle CalendarEvent

---
+++ S16D_Services/CrazyBooks_Models/Models/CalendarEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyBooks_Models.Models
+{
+    public class CalendarEvent
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name ="Date")]
+        public DateTime Date { get; set; }
+    }
+}
